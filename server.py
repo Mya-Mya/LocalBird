276787e13c20787f230668256c72fa5da05d0c87
Mya-Mya <39019907+Mya-Mya@users.noreply.github.com>
2026-08-08 19:26:38 +0900

    Create add page

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ from flask import Flask, jsonify, send_file, abort, render_template, request
 import json
 from repository import *
 import postimporter
+from postimporter import Post, add_images_by_post
+from xscraping import extract_post_from_x_page
 
 app = Flask(__name__)
 
@@ -18,6 +20,46 @@ def index():
     # Get Data
     filenames = repo.list(limit=limit, offset=offset)
     return render_template("list.html", filenames=filenames, page=page)
+
+
+@app.get("/add")
+def get_add():
+    return render_template("add.html")
+
+
+@app.post("/add/from-json")
+def post_add_from_json():
+    try:
+        if request.is_json:
+            data = request.get_json()
+        elif "file" in request.files:
+            data = json.loads(request.files["file"].read().encode("utf-8"))
+        elif "json_text" in request.form:
+            data = json.loads(request.form["json_text"])
+        else:
+            return jsonify({"error": "No JSON data"}), 400
+        post = Post.from_dict(data)
+        filenames = add_images_by_post(repo, post)
+        return jsonify({"filenames": filenames}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+
+@app.post("/add/from-x")
+def post_add_from_x():
+    try:
+        url = None
+        if request.is_json:
+            url = request.get_json().get("url")
+        elif "url" in request.form:
+            url = request.form["url"]
+        if not url:
+            return jsonify({"error": "No URL"}), 400
+        post = extract_post_from_x_page(url)
+        filenames = add_images_by_post(repo, post)
+        return jsonify({"filenames": filenames}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
 
 @app.get("/detail/<string:filename>")
